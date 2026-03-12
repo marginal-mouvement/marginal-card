03834758ac6657984b5289cef32fc997ef260cba
aureRe <aurelien@kraaft.co>
2026-03-12 18:30:25 +0100

    18h26

--- apps/platform/platform-frontend/src/pages/gate/gate.page.tsx
+++ apps/platform/platform-frontend/src/pages/gate/gate.page.tsx
@@ -1,7 +1,10 @@
+import { ArrowUpRightIcon } from "lucide-react";
+
 import { useUser } from "@/modules/auth/useUser.ts";
 import { Header } from "@/parts/header.tsx";
 import { Content } from "@/parts/content.tsx";
 import { Card } from "@/components/ui/card.tsx";
+import { Badge } from "@/components/ui/badge.tsx";
 
 export const GatePage = () => {
   const user = useUser();
@@ -10,13 +13,18 @@ export const GatePage = () => {
     <>
       <Header title={`Bienvenue, ${user.name}`} />
       <Content>
-        <div className="flex h-svh justify-center items-center">
+        <div className="flex h-svh justify-center items-center flex-col gap-4">
           <Card className="p-4">
             <h2 className="text-7xl font-bold text-primary">
               {user.balance}
               <span className="text-lg">pts</span>
             </h2>
           </Card>
+          <Badge asChild>
+            <a href="https://marginalmouvement.com/" target="_blank">
+              Marginal Mouvement <ArrowUpRightIcon />
+            </a>
+          </Badge>
         </div>
       </Content>
     </>
